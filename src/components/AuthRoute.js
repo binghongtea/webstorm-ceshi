db4cbdd0f989b874cd0bcd3d6480fd152f4ee7e9
yangzekai <1500683092@qq.com>
2023-12-25 14:50:40 +0800

    根据是否有token进行跳转

---
+++ src/components/AuthRoute.js
@@ -0,0 +1,16 @@
+//封装高阶组件
+//核心逻辑： 有token 正常跳转 无token 去转到登录页面
+
+import {getToken} from "@/utils";
+import {Navigate} from "react-router-dom";
+
+function AuthRoute({children}){
+    const token = getToken()
+    if(token){
+        return <>{children}</>
+    } else {
+        return <Navigate to={'/login'} replace />
+    }
+}
+
+export {AuthRoute}
