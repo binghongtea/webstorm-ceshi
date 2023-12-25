b271167280d9655493892fc8953cf996b18eca9c
yangzekai <1500683092@qq.com>
2023-12-25 14:29:59 +0800

    axios请求头注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {getToken} from "@/utils/token";
 
 const request = axios.create({
     baseURL: 'http://geek.itheima.net/v1_0',
@@ -6,13 +7,19 @@ const request = axios.create({
 })
 
 // 添加请求拦截器
+//在请求发送前 做拦截 插入一些自定义的配置[参数的处理]
 request.interceptors.request.use((config)=> {
+    const token = getToken()
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, (error)=> {
     return Promise.reject(error)
 })
 
 // 添加响应拦截器
+//在响应返回到客户端之前 做拦截 重点处理返回的数据
 request.interceptors.response.use((response)=> {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
