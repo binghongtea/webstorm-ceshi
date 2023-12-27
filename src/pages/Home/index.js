78adb4e6aea35ca8874c234689fbb59e4b1ac480
yangzekai <1500683092@qq.com>
2023-12-27 15:08:21 +0800

    home中图表初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,32 @@
+import * as echarts from 'echarts';
+import {useEffect, useRef} from "react";
+
 const Home = ()=>{
-    return <div>this is Home</div>
+    const chatRef = useRef(null)
+    useEffect(() => {
+        // 获取DOM结点
+        const chartDom = chatRef.current;
+        // 图表初始化 生成图表实例对象
+        const myChart = echarts.init(chartDom);
+        // 准备图表参数
+        const option = {
+            xAxis: {
+                type: 'category',
+                data: ['Vue', 'React', 'Angular']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [10,80,50],
+                    type: 'bar'
+                }
+            ]
+        };
+        option && myChart.setOption(option);
+    }, []);
+    return <div><div ref={chatRef} style={{width: '500px' ,height : '500px'}}></div></div>
 }
 
 export default Home
